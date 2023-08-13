9d6a8966d8c0e98994219ef79c021078242928ad
RafaelaFontana <74215959+RafaelaFontana@users.noreply.github.com>
2023-08-13 18:01:31 -0300

    front de form e list

--- app/views/quiz/form.php
+++ app/views/quiz/form.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="<?php echo BASEURL; ?>/views/css/listQuiz.css">
     <link rel="stylesheet" href="<?php echo BASEURL; ?>/views/css/cabecalho.css">
     <link rel="stylesheet" href="<?php echo BASEURL; ?>/views/css/index.css">
+    <link rel="stylesheet" href="<?php echo BASEURL; ?>/views/css/form.css">
+
 
 
 </head>
@@ -16,12 +18,12 @@
 
 <body>
 
-    <h3 class="text-center">
+    <h1 class="text-center tituloPagina">
         <?php if ($dados['id'] == 0) echo "Inserir";
         else echo "Alterar";
         ?>
         Quiz
-    </h3>
+    </h1>
 
     <div class="container">
 
@@ -31,12 +33,12 @@
                 <form id="frmQuiz" method="POST" action="<?= BASEURL ?>/controllers/QuizController.php?action=save">
 
                     <div class="form-group">
-                        <label for="txtNomeQuiz">Nome do Quiz:</label>
+                        <label for="txtNomeQuiz" class="nomeAtributo">Nome do Quiz:</label>
                         <input class="form-control" type="text" id="txtNomeQuiz" name="nomeQuiz" maxlength="45" placeholder="Informe o nome do quiz" value="<?php echo (isset($dados["quiz"]) ? $dados["quiz"]->getNomeQuiz() : ''); ?>" />
                     </div>
 
                     <div class="form-group">
-                        <label for="selectZona">Zona:</label>
+                        <label for="selectZona" class="nomeAtributo">Zona:</label>
                         <select class="form-control" id="selectZona" name="zona">
                             <option value="">Selecione a zona</option>
                             <?php foreach ($dados['zonas'] as $zona) : ?>
@@ -48,12 +50,12 @@
                     </div>
 
                     <div class="form-group">
-                        <label for="txtMaximoPergunta">Máximo de perguntas: </label>
+                        <label for="txtMaximoPergunta" class="nomeAtributo">Máximo de perguntas: </label>
                         <input class="form-control" type="number" id="txtMaximoPergunta" name="maximoPergunta" placeholder="Informe o máximo de perguntas do quiz" value="<?php echo (isset($dados["quiz"]) ? $dados["quiz"]->getMaximoPergunta() : ''); ?>" />
                     </div>
 
                     <div class="form-group">
-                        <label for="radComTempo">Com Limite de Tempo:</label>
+                        <label for="radComTempo" class="nomeAtributo">Com Limite de Tempo:</label>
 
                         <!-- 0 representa "não", enquanto 1 representa "sim".
                 Foi optado para ser feito desta maneira por conta do fato de que o atributo comTempo
@@ -61,19 +63,16 @@
 
                         <input type="radio" id="radComTempoSim" name="comTempo" value="1" <?php echo (isset($dados["quiz"])
                                                                                                 && $dados["quiz"]->getComTempo() == 1) ? "checked" : ""; ?>>
-                        <label for="radComTempoSim">Sim</label>
+                        <label for="radComTempoSim" class="nomeAtributo">Sim</label>
                         <input type="radio" id="radComTempoNao" name="comTempo" value="0" <?php echo ((!isset($dados["quiz"])) || (!$dados["quiz"]->getComTempo() == 1)) ? "checked" : ""; ?>>
-                        <label for="radComTempoNao">Não</label>
+                        <label for="radComTempoNao" class="nomeAtributo">Não</label>
                     </div>
 
                     <div class="form-group" id="divQuantTempo" style="<?php echo (isset($dados["quiz"]) && $dados["quiz"]->getComTempo() == 1) ? 'block' : 'none'; ?>">
-                        <label for="txtQuantTempo">Quantidade de Tempo (em minutos): </label>
+                        <label for="txtQuantTempo" class="nomeAtributo">Quantidade de Tempo (em minutos): </label>
                         <input class="form-control" type="number" id="txtQuantTempo" name="quantTempo" placeholder="Informe a quantidade de tempo do quiz" value="<?php echo (isset($dados["quiz"]) ? $dados["quiz"]->getQuantTempo() : ''); ?>" />
                     </div>
 
-                    <a class="btn btn-success btn-block " href="<?= BASEURL ?>/controllers/QuizQuestaoController.php?action=create">
-                        Inserir Questões ao Quiz
-                    </a>
 
 
                     <input type="hidden" id="hddIdQuiz" name="idQuiz" value="<?= $dados['id']; ?>" />
@@ -122,5 +121,3 @@
 </body>
 
 </html>
-
-<
